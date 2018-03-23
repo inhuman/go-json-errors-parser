da345e21e11e9037fa1813ee095af557ce411bed
Ivan <msgexec@gmail.com>
2018-03-23 10:09:14 +0300

    fixed tests

--- parser_test.go
+++ parser_test.go
@@ -14,9 +14,10 @@ func TestParseErrorsExample1(t *testing.T) {
 
 	errs := ParseErrors(string(file))
 
-	assert.Equal(t, 1, errs.GetCount())
+	assert.Equal(t, 2, errs.GetCount())
 	//assert.Equal(t, "data", errs.ParsedErrors[0].Parent)
-	assert.Equal(t, "Package spec not specified", errs.ParsedErrors[0].Children["PACKAGE_SPEC"][0])
+	assert.Equal(t, "Validations failed for package 'c4b10faf-62f9-4b75-ae7f-9dc042e3d310'. Error(s): [Validation failed.]. Please correct and resubmit.", errs.ParsedErrors[0].Message)
+	assert.Equal(t, "Package spec not specified", errs.ParsedErrors[1].Children["PACKAGE_SPEC"][0])
 }
 
 func TestParseErrorsExample2(t *testing.T) {
@@ -38,8 +39,8 @@ func TestParseErrorsExample3(t *testing.T) {
 	errs := ParseErrors(string(file))
 
 	//assert.Equal(t, "data", errs.ParsedErrors[0].Parent)
-	assert.Equal(t, "A pipeline must have at least one material", errs.ParsedErrors[0].Children["materials"][0])
-	assert.Equal(t, "Invalid label '123'. Label should be composed of alphanumeric text, it can contain the build number as ${COUNT}, can contain a material revision as ${<material-name>} of ${<material-name>[:<number>]}, or use params as #{<param-name>}.", errs.ParsedErrors[0].Children["label_template"][0])
+	assert.Equal(t, "A pipeline must have at least one material", errs.ParsedErrors[1].Children["materials"][0])
+	assert.Equal(t, "Invalid label '123'. Label should be composed of alphanumeric text, it can contain the build number as ${COUNT}, can contain a material revision as ${<material-name>} of ${<material-name>[:<number>]}, or use params as #{<param-name>}.", errs.ParsedErrors[1].Children["label_template"][0])
 }
 
 func TestParseErrorsExample4(t *testing.T) {
@@ -50,7 +51,7 @@ func TestParseErrorsExample4(t *testing.T) {
 	errs := ParseErrors(string(file))
 
 	//assert.Equal(t, "data", errs.ParsedErrors[0].Parent)
-	assert.Equal(t, "A pipeline must have at least one material", errs.ParsedErrors[0].Children["materials"][0])
+	assert.Equal(t, "A pipeline must have at least one material", errs.ParsedErrors[1].Children["materials"][0])
 }
 
 func TestParseErrorsExample5(t *testing.T) {
@@ -63,7 +64,7 @@ func TestParseErrorsExample5(t *testing.T) {
 	//assert.Equal(t, "materials", errs.ParsedErrors[0].Parent)
 	assert.Equal(t, 0, len(errs.ParsedErrors[0].Message))
 
-	assert.Equal(t, "Invalid Destination Directory. Every material needs a different destination directory and the directories should not be nested.", errs.ParsedErrors[0].Children["destination"][0])
+	assert.Equal(t, "Invalid Destination Directory. Every material needs a different destination directory and the directories should not be nested.", errs.ParsedErrors[1].Children["destination"][0])
 }
 
 func TestParseErrorsExample6(t *testing.T) {
